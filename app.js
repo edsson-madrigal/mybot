6e04cebcb75bc3a9198ead3def26a6021df11c95
Edsson Madrigal <akairu@hotmail.com>
2020-04-02 18:31:08 -0600

    code changes

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
  
 app.post('/webhook/', function(req, res) {
     var data = req.body;
